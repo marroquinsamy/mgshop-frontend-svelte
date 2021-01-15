3dae7034f69c53cf3da7bd23001043881a0aacb9
Steven Samuel Marroquín González <samy23033@gmail.com>
2021-01-15 11:13:20 -0600

    Se añadió la URL del backend

--- src/Products/services/productsService.ts
+++ src/Products/services/productsService.ts
@@ -1,7 +1,8 @@
 // Scripts
 import type { IProduct } from '../models/Product'
 
-export const API = 'http://192.168.0.107:4000'
+// export const API = 'http://192.168.0.107:4000'
+export const API = 'https://mgshop-backend.herokuapp.com'
 
 export const errorSeparator: string = '$$$' // Use this constant to separate the res status code, the error title and the error description
 
