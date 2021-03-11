499452eefb36edf413d8120e83c1feb8b06b2ff5
ssmg <ssmg.sg@gmail.com>
2021-03-11 16:11:39 -0600

    Remove logic and just assign listing items task

--- src/products/components/ProductsList.svelte
+++ src/products/components/ProductsList.svelte
@@ -8,54 +8,24 @@
   import { useFocus } from 'svelte-navigator'
 
   // Scripts
-  import { getProducts } from '../services/productsService'
-  import type { IProduct } from '../models/Product'
   import { title } from '../../stores/title'
+  import type { IProduct } from '../models/Product'
+
+  export let products: IProduct[]
 
   const registerFocus = useFocus()
-
-  const loadProducts = async (): Promise<IProduct[]> => {
-    const productsResponse: IProduct[] = await getProducts()
-    return sortProducts(productsResponse)
-  }
-
-  const sortProducts = (products: IProduct[]): IProduct[] => {
-    const formatedProducts: IProduct[] = products
-      .map((product) => {
-        return {
-          ...product,
-          createdAt: product.createdAt
-            ? new Date(product.createdAt)
-            : new Date(),
-          updatetAt: product.updatedAt
-            ? new Date(product.updatedAt)
-            : new Date(),
-        }
-      })
-      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-
-    return formatedProducts
-  }
-
-  const sortedProducts: Promise<IProduct[]> = loadProducts()
 </script>
 
 <svelte:head>
   <title>Productos | {$title}</title>
 </svelte:head>
 
-{#await sortedProducts}
-  <ProductsLoader text="Cargando productos" />
-{:then products}
-  <h2 class="products-page--page-title" use:registerFocus>Productos</h2>
-  <div class="products-container">
-    {#each products as product}
-      <ProductItem {product} />
-    {/each}
-  </div>
-{:catch error}
-  <ErrorPage message={error.message} />
-{/await}
+<h2 class="products-page--page-title" use:registerFocus>Productos</h2>
+<div class="products-container">
+  {#each products as product}
+    <slot {product} />
+  {/each}
+</div>
 
 <style>
   .products-container {
