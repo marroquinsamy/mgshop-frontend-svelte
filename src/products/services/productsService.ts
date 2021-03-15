daa75656d1cbaa8d096d1fdb52b44f20c4c51d65
ssmg <ssmg.sg@gmail.com>
2021-03-15 15:05:48 -0600

    Modify methods Make not recursive the getImage function Delete getProduct function and make getProducts double purpose

--- src/products/services/productsService.ts
+++ src/products/services/productsService.ts
@@ -4,10 +4,10 @@ import config from '../../config/config'
 
 export const errorSeparator: string = '$$$' // Use this constant to separate the res status code, the error title and the error description
 
-export const getProducts = async () => {
+export const getProducts = async (id?: string) => {
   let res: Response
   try {
-    res = await fetch(`${config.API}/products`)
+    res = await fetch(`${config.API}/products/${id ? id : ''}`)
   } catch (error) {
     console.log(error)
     throw new Error(
@@ -15,11 +15,15 @@ export const getProducts = async () => {
     )
   }
 
-  if (res.status === 204)
+  if (res.status === 204) {
     throw new Error(
       `204${errorSeparator}¡Ups! Parece que olvidamos algo${errorSeparator}Comunícate con nosotros para verificar la disponibilidad de nuestros productos.`
     )
-  else if (res.ok) {
+  } else if (res.status === 404) {
+    throw new Error(
+      `404${errorSeparator}Parece que alguien no vino a trabajar hoy${errorSeparator}No hemos encontrado el item que estás buscando. Haz click en el botón para explorar nuestro catálogo de productos.`
+    )
+  } else if (res.ok) {
     const products: IProduct[] = await res.json()
     return products
   } else
@@ -28,57 +32,25 @@ export const getProducts = async () => {
     )
 }
 
-export const getProduct = async (id: string) => {
+/**
+ *
+ * @param {string} product The path in the server where the image is storaged. If given and isID is passed as true, it will fetch the product first and then it will fetch the picture
+ * @param {boolean} isID If marked as true, it will fetch the product first and then the picture
+ */
+export const getImage = async (product: IProduct) => {
   let res: Response
   try {
-    res = await fetch(`${config.API}/products/${id}`)
+    res = await fetch(`${config.API}/${product.imagePath}`)
   } catch (error) {
     console.log(error)
-    throw new Error(
-      `000${errorSeparator}Hubo un error obteniendo el producto que nos solicitaste${errorSeparator}Intenta de nuevo más tarde o ponte en contacto con nosotros mediante nuestras redes sociales.`
-    )
+    throw new Error()
   }
 
   if (res.ok) {
-    const product: IProduct = await res.json()
-    return product
-  } else if (res.status === 404)
-    throw new Error(
-      `404${errorSeparator}Parece que alguien no vino a trabajar hoy${errorSeparator}No hemos encontrado el item que estás buscando. Haz click en el botón para explorar nuestro catálogo de productos.`
-    )
-  else
-    throw new Error(
-      `000${errorSeparator}Hubo un error obteniendo el producto que nos solicitaste${errorSeparator}Intenta de nuevo más tarde o ponte en contacto con nosotros mediante nuestras redes sociales.`
-    )
-}
-
-/**
- *
- * @param {string} imagePathOrID The path in the server where the image is storaged. If given and isID is passed as true, it will fetch the product first and then it will fetch the picture
- * @param {boolean} isID If marked as true, it will fetch the product first and then the picture
- */
-export const getImage = async (
-  imagePathOrID: string,
-  isID: boolean = false
-) => {
-  if (isID) {
-    const product: Promise<IProduct> = getProduct(imagePathOrID)
-    return getImage((await product).imagePath)
+    const image = await res.blob()
+    const outside = URL.createObjectURL(image)
+    return outside
   } else {
-    let res: Response
-    try {
-      res = await fetch(`${config.API}/${imagePathOrID}`)
-    } catch (error) {
-      console.log(error)
-      throw new Error()
-    }
-
-    if (res.ok) {
-      const image = await res.blob()
-      const outside = URL.createObjectURL(image)
-      return outside
-    } else {
-      throw new Error()
-    }
+    throw new Error()
   }
 }
