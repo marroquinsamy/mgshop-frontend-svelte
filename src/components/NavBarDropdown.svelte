014407907438651c87082070e0d979374c51f9ae
ssmg <ssmg.sg@gmail.com>
2021-03-16 08:13:48 -0600

    Add inset shadow

--- src/components/NavBarDropdown.svelte
+++ src/components/NavBarDropdown.svelte
@@ -35,6 +35,7 @@
 
     box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px,
       rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
+    box-shadow: var(--shadow-outset-3), var(--shadow-inset-1);
 
     padding: 7px;
     border-radius: 0.5em;
